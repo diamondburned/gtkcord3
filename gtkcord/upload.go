61290347817519ec8613b603a898703726dfb074
diamondburned (Forefront) <datutbrus@gmail.com>
2020-02-15 21:29:58 -0800

    Bump Arikawa version

--- gtkcord/upload.go
+++ gtkcord/upload.go
@@ -1,3 +1,37 @@
 package gtkcord
 
-// func NewUploader() (error
+import (
+	"github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/gtk"
+)
+
+type Uploader struct {
+	*gtk.FileChooserNativeDialog
+	callback func(string)
+}
+
+func NewUploader(callback func(path string)) (*Uploader, error) {
+	dialog := must(
+		gtk.FileChooserNativeDialogNew, "Upload File",
+		App.Window, gtk.FILE_CHOOSER_ACTION_OPEN, "Upload", "Cancel").(*gtk.FileChooserNativeDialog)
+	must(dialog.SetCurrentFolder, glib.GetUserDataDir())
+	must(dialog.Connect, "response", func() {
+		dialog.Destroy()
+	})
+
+	return &Uploader{
+		FileChooserNativeDialog: dialog,
+		callback:                callback,
+	}, nil
+}
+
+// Spawn should be running in a goroutine.
+func (u *Uploader) Spawn() {
+	must(u.Show)
+
+	resCode := must(u.Run).(int)
+	if gtk.ResponseType(resCode) != gtk.RESPONSE_ACCEPT {
+		return
+	}
+
+}
